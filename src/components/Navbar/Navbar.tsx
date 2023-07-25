a33b34c29a8dd0d9f5b93a7818d1eb499da1d85e
Dias Abdrakhmanov <abdrahmanov.03@inbox.ru>
2023-07-25 18:05:00 +0600

    burger menu added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,36 +1,55 @@
 import { NavbarProps } from "./Navbar.props";
 import styles from "./Navbar.module.css";
-import {useState} from "react";
+import { useState, useEffect } from "react";
 
 function Navbar({ childrenList, setDisplay }: NavbarProps): JSX.Element {
+  const [menu, setMenu] = useState(false);
+  const [screen, setScreen] = useState(window.innerWidth);
 
-  const [menu, setMenu] = useState(false)
-  const [screen, setScreen] = useState(window.innerWidth)
+  useEffect(() => {
+    const handleResize = () => {
+      setScreen(window.innerWidth);
+    };
 
-  window.addEventListener("resize", () => setScreen(window.innerWidth));
+    window.addEventListener("resize", handleResize);
 
-  if (screen > 998) {
-    setDisplay(true)
-  }
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty dependency array to run this effect only once on mount
+
+  useEffect(() => {
+    if (screen > 998) {
+      setMenu(false)
+      setDisplay(true);
+    }
+  }, [screen, setDisplay]);
 
   const menuHandler = () => {
-    setMenu(prev => !prev)
-    setDisplay(prev => !prev)
-  }
+    setMenu((prev) => !prev);
+    setDisplay((prev) => !prev);
+  };
 
   return (
-
     <>
-    <ul className={styles.navbar}>
-      {childrenList.map((e: JSX.Element) => (
-        <li key={e.props.children}>{e}</li>
-      ))}
-    </ul>
-    <div onClick={menuHandler} className={styles.burger_menu}>
+      {(
+        <ul
+          style={{
+            display: menu ? "flex" : ""
+          }}
+          className={styles.navbar}
+        >
+          {childrenList.map((e: JSX.Element) => (
+            <li key={e.props.children}>{e}</li>
+          ))}
+        </ul>
+      )}
+      <div onClick={menuHandler} className={` ${styles.burger_menu} ${menu ? styles.burger_menu_open : ''}`}>
         <span></span>
         <span></span>
         <span></span>
-    </div>
+      </div>
     </>
   );
 }
